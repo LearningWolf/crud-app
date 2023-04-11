4535d4724ae2612c4678c93f5bf32a07761076f8
Keshav <keshav.kumar@corporateserve.com>
2023-04-11 16:46:09 +0530

    crud home

---
+++ src/app/crudhome/crudhome.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrudhomeComponent } from './crudhome.component';
+
+describe('CrudhomeComponent', () => {
+  let component: CrudhomeComponent;
+  let fixture: ComponentFixture<CrudhomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CrudhomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CrudhomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
